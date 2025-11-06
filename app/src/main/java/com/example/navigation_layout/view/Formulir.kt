ce5e1627e04ec68d81a940b49efbc9827b512752
MalkingMan <arrayalkhozini@gmail.com>
2025-11-06 17:07:47 +0700

    Menambahkan TopAppBar di Formulir

--- app/src/main/java/com/example/navigation_layout/view/Formulir.kt
+++ app/src/main/java/com/example/navigation_layout/view/Formulir.kt
@@ -5,3 +5,14 @@ fun FormIsian(
     jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick: () -> Unit
 ) {
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    )
+    }
