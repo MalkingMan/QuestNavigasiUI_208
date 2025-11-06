9a9fcd6c8a11db95c90fbc5da5b76e865d5a707d
MalkingMan <arrayalkhozini@gmail.com>
2025-11-06 09:36:16 +0700

    Memperbaiki padding dan menambahkan fungsi back

--- app/src/main/java/com/example/navigation_layout/Navigasi.kt
+++ app/src/main/java/com/example/navigation_layout/Navigasi.kt
@@ -1,10 +1,13 @@
 package com.example.navigation_layout
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 
 enum class Navigasi {
@@ -23,7 +26,7 @@ fun DataApp(
             navController = navController,
             startDestination = Navigasi.Formulir.name,
 
-            modifier = Modifier.padding(paddingValues = isiRuang)){
+            modifier = Modifier.padding( paddingValues = isiRuang)){
             composable(route = Navigasi.Formulir.name){
                 FormIsian(
                     onSubmitBtnClick = {
@@ -40,3 +43,10 @@ fun DataApp(
 
     }
 }
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulir.name, inclusive = false)
+}
+
