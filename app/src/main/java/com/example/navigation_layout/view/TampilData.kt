d7718abee93da1577c942f8e03fce232176b7340
MalkingMan <arrayalkhozini@gmail.com>
2025-11-06 17:40:40 +0700

    Memperbarui TampilData.kt

--- app/src/main/java/com/example/navigation_layout/view/TampilData.kt
+++ app/src/main/java/com/example/navigation_layout/view/TampilData.kt
@@ -20,5 +20,14 @@ fun TampilData(
                 )
             )
         }
-    )
-}
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.padding(all = dimensionResource(id = R.dimen.padding_medium)),
+                verticalArrangement = Arrangement.spacedBy(
+                    space = dimensionResource(id = R.dimen.padding_small)
+                )
+            ) {
