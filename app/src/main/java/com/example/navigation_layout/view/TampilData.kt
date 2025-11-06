f35ef038e104c8005e13912c8996686ffdfe8c52
MalkingMan <arrayalkhozini@gmail.com>
2025-11-06 17:40:04 +0700

    Membuat halaman Tampil Data

--- app/src/main/java/com/example/navigation_layout/view/TampilData.kt
+++ app/src/main/java/com/example/navigation_layout/view/TampilData.kt
@@ -5,3 +5,20 @@ package com.example.navigation_layout.view
 fun TampilData(
     onBackBtnClick: () -> Unit
 ) {
+    val items = listOf(
+        Pair(first = stringResource(id = R.string.nama_lengkap), second = "Contoh Nama"),
+        Pair(first = stringResource(id = R.string.jenis_kelamin), second = "Lainnya"),
+        Pair(first = stringResource(id = R.string.alamat), second = "Yogyakarta")
+    )
+
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.tampil), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700)
+                )
+            )
+        }
+    )
+}
