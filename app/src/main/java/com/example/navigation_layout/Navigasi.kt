0cdb6ff87c7434739843982a82248b36062483f7
MalkingMan <arrayalkhozini@gmail.com>
2025-11-06 09:20:34 +0700

    Membuat file navigasi dan enum class Navigasi

---
+++ app/src/main/java/com/example/navigation_layout/Navigasi.kt
@@ -0,0 +1,26 @@
+package com.example.navigation_layout
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir
+    }
+}
